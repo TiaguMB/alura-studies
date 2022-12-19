af75e7642c43ca9cb8979ecccf18eb486decdc47
TiaguMB <tiagomb.privado@gmail.com>
2022-12-19 18:25:15 -0300

    feat: remove comments

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import style from '../Lista.module.scss';
 
 export default function Item({ tarefa, tempo }: { tarefa: string, tempo: string }) {
-// export default function Item(props: { tarefa: string, tempo: string }) {
-	// const { tarefa, tempo } = props;
 	return (
 		<li className={ style.item }>
 			<h3>{ tarefa }</h3>
