2458dac57c53af2fa6fdf67cec64b65c09339a43
TiaguMB <tiagomb.privado@gmail.com>
2022-12-20 11:17:26 -0300

    feat: add example of useState in component Lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-	const tarefas = [{
+	const [ tarefas, setTarefas ] = useState([{
 		tarefa: 'React',
 		tempo: '02:00:00'
 	}, {
@@ -11,11 +12,13 @@ function Lista() {
 	}, {
 		tarefa: 'TypeScript',
 		tempo: '03:00:00'
-	}];
+	}]);
 
 	return (
 		<aside className={ style.ListaTarefas }>
-			<h2>Estudos do dia</h2>
+			<h2 onClick={() => {
+				setTarefas([...tarefas, { tarefa: 'Estudar estado', tempo: '05:00:00' }]);
+			}}>Estudos do dia</h2>
 			<ul>
 				{tarefas.map((item, index) => (
 					<Item
